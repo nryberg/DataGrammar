37eba702dd3b9563ada8952676fd3c60b568b24e
Nick Ryberg <nryberg@gmail.com>
2017-02-19 14:59:38 -0600

    Data encoded into Bolt DB

--- load_csv.go
+++ load_csv.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/boltdb/bolt"
 	"github.com/gocarina/gocsv"
 )
 
 // Entry is a single line of a database/table definition
 type Entry struct { // Our example struct, you can use "-" to ignore a field
-	System string `csv:"system"`
-	Schema string `csv:"schema"`
-	Table  string `csv:"table"`
-	Column string `csv:"column"`
+	Database  string `csv:"database"`
+	System    string `csv:"system"`
+	Schema    string `csv:"schema"`
+	Table     string `csv:"table"`
+	Column    string `csv:"column"`
+	Ordinal   int    `csv:"Ordinal"`
+	Type      string `csv:"Type"`
+	Length    int    `csv:"Length"`
+	Precision int    `csv:"Precision"`
+	Scale     int    `csv:"Scale"`
 }
 
 // UID returns a unique identifier for the entry
@@ -21,6 +30,8 @@ func (e *Entry) UID() string {
 }
 
 func main() {
+
+	// Load Definition file from csv
 	definitionFile, err := os.OpenFile("psqlmetadata.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
@@ -36,8 +47,31 @@ func main() {
 		fmt.Println("Hello", entry.UID())
 	}
 
-	if _, err = definitionFile.Seek(0, 0); err != nil { // Go to the start of the file
-		panic(err)
+	// Send Entries to Bolt DB
+
+	db, err := bolt.Open("datagrammar.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer db.Close()
+
+	db_name := entries[0].Database
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(db_name))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		for _, entry := range entries {
+			encoded, err := json.Marshal(entry)
+			if err != nil {
+				return err
+			}
+
+			err = b.Put([]byte(entry.UID()), encoded)
+		}
+
+		return nil
+	})
 
 }
