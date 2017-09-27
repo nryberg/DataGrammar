42f080ba1f10e330a5781071b02a0873d6dd20a8
Nick Ryberg <nryberg@gmail.com>
2017-09-26 22:27:26 -0500

    Using correct format for Psql

--- db_meta_sql/postgresql_meta.sql
+++ db_meta_sql/postgresql_meta.sql
@@ -1,3 +1,16 @@
-SELECT SELECT *
-FROM pg_settings
-WHERE name = 'port';
+SELECT 'localhost' as server, -- replace as needed
+      table_catalog as database,
+      'Postgresql' as type,
+      'System Name' as system, -- typically the source system name
+      TABLE_SCHEMA as schema,
+      TABLE_NAME as table,
+      column_name as column,
+      ordinal_position as ordinal,
+      data_type as type,
+      character_maximum_length as length,
+      numeric_precision as precision,
+      numeric_scale as scale
+
+FROM  information_schema.columns
+WHERE table_schema = 'public'
+ORDER BY TABLE_NAME, column_name ASC
